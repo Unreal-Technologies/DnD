82696e2c201ecd7834efcd5a3d7071f99e175e83
Peter Overeijnder <peterovereijnder@hotmail.com>
2021-05-25 21:11:09 +0200

    -

--- JsLibrary/src/Corruption.js
+++ JsLibrary/src/Corruption.js
@@ -2,62 +2,63 @@ var Corruption = Corruption || (function () {
     'use strict';
 
     var attributeHash = {
-            "corruption": {
-                "current": 0,
-                "max": 100
-            }
-        },
+        "corruption": {
+            "current": 0,
+            "max": 100
+        }
+    },
 
-        addAttributes = function(characterID, attributeHash) {
-            for (var key in attributeHash) {
-                if (attributeHash.hasOwnProperty(key)) {
-                    var foundAttribute = findObjs({
-                        _characterid: characterID,
-                        name: key
-                    })[0];
-                        
-                    if (!foundAttribute) {
-                        log("Attribute " + key + " not found for character ID " + characterID + " Creating.");
-                        createObj("attribute", {
-                            name: key,
-                            current: attributeHash[key]["current"],
-                            max: attributeHash[key]['max'],
-                            characterid: characterID
-                        });
-                    }
+    addAttributes = function(characterID, attributeHash) {
+        for (var key in attributeHash) {
+            if (attributeHash.hasOwnProperty(key)) {
+                var foundAttribute = findObjs({
+                    _characterid: characterID,
+                    name: key
+                })[0];
+
+                if (!foundAttribute) {
+                    log("Attribute " + key + " not found for character ID " + characterID + " Creating.");
+                    createObj("attribute", {
+                        name: key,
+                        current: attributeHash[key]["current"],
+                        max: attributeHash[key]['max'],
+                        characterid: characterID
+                    });
                 }
             }
-        },
+        }
+    },
 
-        initCharacterAttributes = function(char){
-            addAttributes(char.id, attributeHash);
-        },
-        
-        handleInput = function(msg) {
-            if(msg.type === "api" && msg.content === "!initcorruption") {
-                log("Initializing attributes for all existing characters.");
-                var allCharacters = findObjs({
-                    _type: "character"
-                });
-                _.each(allCharacters, function(char) {
-                    initCharacterAttributes(char);
-                });
-            }
-        },
+    initCharacterAttributes = function(char){
+        addAttributes(char.id, attributeHash);
+    },
 
-        registerEventHandlers = function() {    
-            on("add:character", initCharacterAttributes);
-            on("chat:message", handleInput);
-        };
+    handleInput = function(msg) {
+        if(msg.type === "api" && msg.content === "!initcorruption") {
+            log("Initializing attributes for all existing characters.");
+            var allCharacters = findObjs({
+                _type: "character"
+            });
+            _.each(allCharacters, function(char) {
+                initCharacterAttributes(char);
+            });
+        }
+    },
+
+    registerEventHandlers = function() {    
+        on("add:character", initCharacterAttributes);
+        on("chat:message", handleInput);
+    };
     
-    return {
-        RegisterEventHandlers: registerEventHandlers
+    return 
+    {
+        RegisterEventHandlers: registerEventHandlers;
     };    
     
 })();
 
-on("ready", function() {
+on("ready", function() 
+{
     'use strict';
-    
     Corruption.RegisterEventHandlers();    
 });
