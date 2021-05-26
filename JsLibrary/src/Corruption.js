d38c4b2cb8ee60afc5e71f2cb655bce3a140c330
Peter Overeijnder <peterovereijnder@hotmail.com>
2021-05-26 22:29:32 +0200

    -

--- JsLibrary/src/Corruption.js
+++ JsLibrary/src/Corruption.js
@@ -89,17 +89,70 @@ var Corruption = Corruption || (function () {
         return previous === null ? 'N' : (previous[k1][k2] === current[k1][k2] ? 'N' : (previous[k1][k2] < current[k1][k2] ? 'U' : 'D'));
     },
 
+    levelSpecificPassives = function(level)
+    {
+        if(level >= 20)
+        {
+            return {
+                'tier': 5,
+                'turns': null,
+                'dice': null
+            };
+        }
+        else if(level >= 16)
+        {
+            return {
+                'tier': 4,
+                'turns': 20,
+                'dice': '1d4'
+            };
+        }
+        else if(level >= 13)
+        {
+            return {
+                'tier': 3,
+                'turns': 15,
+                'dice': '1d6'
+            };
+        }
+        else if(level >= 10)
+        {
+            return {
+                'tier': 2,
+                'turns': 10,
+                'dice': '1d8'
+            };
+        }
+        else if(level >= 7)
+        {
+            return {
+                'tier': 1,
+                'turns': 5,
+                'dice': '1d10'
+            };
+        }
+        return {
+            'tier': 0,
+            'turns': null,
+            'dice': null
+        };
+    },
+
     attributesChanged = function(charId, attributes, previous)
     {
         var hpDirection = attributeDirection(attributes, previous, 'hp', 'current');
         var corruptionDirection = attributeDirection(attributes, previous, 'corruption', 'current');
         
-        log('ATTR CHANGED!');
-        log(charId);
-        log(attributes);  
-        log(previous);
-        log(hpDirection);
-        log(corruptionDirection);
+        var level = attributes['level'];
+        var hp = attributes['hp']['current'];
+        var corruption = attributes['corruption']['current'];
+        
+        var passives = levelSpecificPassives(level);
+        
+        if(passives['tier'] !== 0 && corruptionDirection === 'U')
+        {
+           log(passives);
+        }
     },
 
     progressionLoop = function()
@@ -113,7 +166,7 @@ var Corruption = Corruption || (function () {
             if(attributes['enabled'] && (previous === undefined || previous === null || prevAttributes[charId] !== hash))
             {
                 var previousParsed = previous === undefined || previous === null ? null : JSON.parse(previous);
-                
+
                 attributesChanged(charId, attributes, previousParsed);
                 prevAttributes[charId] = hash;
             }
