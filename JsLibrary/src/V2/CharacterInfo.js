ea57e4d1372d8e38a317c23b3a55cad6261e31fd
Peter Overeijnder <peterovereijnder@hotmail.com>
2021-05-30 21:46:25 +0200

    -

--- JsLibrary/src/V2/CharacterInfo.js
+++ JsLibrary/src/V2/CharacterInfo.js
@@ -1,6 +1,7 @@
 var CharacterInfo =
 {
     preloadCharactersEvents: [],
+    attributeUpdateEvents: [],
     characters: [],
     data: [],
     
@@ -14,6 +15,8 @@ var CharacterInfo =
         {
             self._PreloadCharacters(char);
         });
+        
+        this._CharacterChecker();
     },
     
     Data: function(characterId)
@@ -31,19 +34,164 @@ var CharacterInfo =
         this._PreloadCharacters(char);
     },
     
+    RegisterOnAtributeUpdate: function(callback)
+    {
+        this.attributeUpdateEvents[this.attributeUpdateEvents.length] = callback;
+    },
+    
     RegisterOnPreloadCharacters: function(callback)
     {
         this.preloadCharactersEvents[this.preloadCharactersEvents.length] = callback;
+    },
+    
+    _CharacterChecker: function()
+    {
+        var self = this;
+        _.each(this.characters, function(charId)
+        {
+            if(self.data.hasOwnProperty(charId))
+            {
+                var data = self.data[charId];
+                var char = data['char'];
+                
+                var attributeInfo = self._LoadCharacterAttributes(char);
+                var newAttributes = attributeInfo['attributes'];
+                var oldAttributes = data['attributes'];
+
+                var changed = [];
+                var hasChanges = false;
+                for(const k in oldAttributes)
+                {
+                    if(!newAttributes.hasOwnProperty(k))
+                    {
+                        log('CALL 1');
+                        _.each(self.attributeUpdateEvents, function(event)
+                        {
+                            event(charId, oldAttributes[k], null);
+                        });
+                        changed[changed.length] = k;
+                        hasChanges = true;
+                    }
+                    else if(oldAttributes[k].get('current') !== newAttributes[k].get('current') || oldAttributes[k].get('max') !== newAttributes[k].get('max'))
+                    {
+                        log('CALL 2');
+                        _.each(self.attributeUpdateEvents, function(event)
+                        {
+                            event(charId, oldAttributes[k], newAttributes[k]);
+                        });
+                        changed[changed.length] = k;
+                        hasChanges = true;
+                    }
+                }
+                for(const k in newAttributes)
+                {
+                    var idx = changed.indexOf(k);
+                    if(idx !== -1)
+                    {
+                        continue;
+                    }
+                    else if(!oldAttributes.hasOwnProperty(k))
+                    {
+                        _.each(self.attributeUpdateEvents, function(event)
+                        {
+                            event(charId, null, newAttributes[k]);
+                        });
+                        hasChanges = true;
+                    }
+                    else if(oldAttributes[k].get('current') !== newAttributes[k].get('current') || oldAttributes[k].get('max') !== newAttributes[k].get('max'))
+                    {
+                        _.each(self.attributeUpdateEvents, function(event)
+                        {
+                            event(charId, oldAttributes[k], newAttributes[k]);
+                        });
+                        hasChanges = true;
+                    }
+                }
+                
+                if(hasChanges)
+                {
+                    log(hasChanges);
+                }
+            }
+        });
+
+        setTimeout(function()
+        {
+            self._CharacterChecker();
+        }, 500);
+    },
+    
+    _LoadCharacterAttributes: function(char)
+    {
+        var equipment = {};
+        var attributes = {};
+        _.each(findObjs({
+            _type: 'attribute',
+            _characterid: char.id
+        }), function(attribute)
+        {
+            var name = attribute.get('name');
+            if((new RegExp('^repeating_inventory_').test(name)) || (new RegExp('^repeating_attack_')).test(name))
+            {
+                equipment[name] = attribute;
+            }
+            else
+            {
+                attributes[name] = attribute;
+            }
+        });
+        
+        return {
+            'equipment': equipment,
+            'attributes': attributes
+        };
+    },
+    
+    _LoadCharacterEquipment: function(equipmentData)
+    {
+        var weapons = [];
+        _.each(equipmentData, function(attribute)
+        {
+            if((new RegExp('^repeating_inventory_.+_itemattackid$')).test(attribute.get('name')))
+            {
+                var weapData = {
+                    'id': attribute.get('current'),
+                    'name': null,
+                    'range': null
+                };
+                _.each(equipmentData, function(subAttr)
+                {
+                    var attrName = subAttr.get('name');
+                    if((new RegExp('^repeating_attack_'+weapData['id']+'_.*$')).test(attrName))
+                    {
+                        if((new RegExp('.*_atkname$')).test(attrName))
+                        {
+                            weapData['name'] = subAttr.get('current');
+                        }
+                        else if((new RegExp('.*_atkrange$')).test(attrName))
+                        {
+                            weapData['range'] = parseInt(subAttr.get('current'));
+                        }
+                    }
+                });
+                weapons[weapons.length] = weapData;
+            } 
+        });
+        
+        return weapons;
     },
     
     _LoadCharacterData: function(char)
     {
         var id = char.id;
         var tokenObject = null;
+        var attributeInfo = this._LoadCharacterAttributes(char);
 
         this.data[id] = {
             'char': char,
-            'token': tokenObject
+            'token': tokenObject,
+            'attributes': attributeInfo['attributes'],
+            'equipment': this._LoadCharacterEquipment(attributeInfo['equipment'])
         };
         
         var self = this;
--- JsLibrary/src/V2/Corruption.js
+++ JsLibrary/src/V2/Corruption.js
@@ -6,7 +6,16 @@ var Corruption =
         
         log('CORRUPTION PRELOAD '+charId);
         log(data);
+    },
+    
+    AttributesChanged: function(charId, old_, new_)
+    {
+        log('CORRUPTION ATTR CHANGED '+charId);
+        log(charId);
+        log(old_);
+        log(new_);
     }
 };
 
 CharacterInfo.RegisterOnPreloadCharacters(function(charId){ Corruption.PreloadCharacter(charId); });
+CharacterInfo.RegisterOnAtributeUpdate(function(charId, old_, new_) { Corruption.AttributesChanged(charId, old_, new_); });
